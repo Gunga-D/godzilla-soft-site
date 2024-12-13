1ed1cc7a613190d5b882783ee6e4c8234d54ab1c
Марина <133772518+marinakokovihina@users.noreply.github.com>
2024-12-14 00:34:49 +0300

    add default

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from "./layout/header/Heder";
+import {Container} from "./styles/Container";
+
+function App() {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  );
+}
+
+export default App;
