a3d97e8699b9f8f5b6abf4297b17474615c61f51
Марина <133772518+marinakokovihina@users.noreply.github.com>
2025-02-05 23:02:22 +0300

    add CONTACTS

--- src/layout/Contacts/Contacts.tsx
+++ src/layout/Contacts/Contacts.tsx
@@ -5,13 +5,17 @@ export const Contacts = () => {
     return (
         <StyledContacts>
             <StyledWrap>
-                ИП Дондоков Гунга Базарович<br/>ИНН 540618782712<br/>ОГРН/ОГРНИП 324774600067364<br/><br/>Режим работы поддержки<br/>Ежедневно с 10:00 до 20:00<br/><br/>Адрес электронной почты<br/>godzilla-soft@yandex.ru<br/><br/>Номера телефонов<br/>+7 (925) 313 80 11<br/>+7 (999) 134 72 87<br/>+7 (901) 752 35 98
+                ИП Дондоков Гунга Базарович<br/>
+                ИНН 540618782712<br/>ОГРН/ОГРНИП 324774600067364<br/><br/>Режим работы поддержки<br/>Ежедневно с 10:00 до 20:00<br/><br/>Адрес электронной почты<br/>godzilla-soft@yandex.ru<br/><br/>Номера телефонов<br/>+7 (925) 313 80 11<br/>+7 (999) 134 72 87<br/>+7 (901) 752 35 98
             </StyledWrap>
         </StyledContacts>
     );
 };
 
-// @ts-ignore
+const StyledContacts = styled.div `
+  margin-top: 200px;
+`
+
 const StyledWrap = styled.p `
   color: white;
   font-style: normal;
@@ -19,9 +23,4 @@ const StyledWrap = styled.p `
   font-size: 24px;
   line-height: 150%;
   text-align: center;
-  
-`;
-// @ts-ignore
-const StyledContacts = styled.div `
-  margin-top: 200px;
 `;
