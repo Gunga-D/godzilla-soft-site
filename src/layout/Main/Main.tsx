e7cf0fa6ec39ae36aba51eec62ab183a943468f7
Марина <133772518+marinakokovihina@users.noreply.github.com>
2025-02-16 02:15:26 +0300

    баннер

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -8,7 +8,7 @@ export const Main = () => {
     return (
         <Container>
             <MainSection/>
-            <SeasonSection mainTitle={"Пополни стим своему другу"} url={"/items?limit=100&category_id=10004"}/>
+            <SeasonSection mainTitle={"Подарок твоему другу"} url={"/sales_items"}/>
             <BaseSection mainTitle={"Новинки"} url={"/new_items"}/>
             <BaseSection mainTitle={"Выбор редакции "} url={"/recomendation_items"}/>
         </Container>
