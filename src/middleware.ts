99265dcfee8f43909411e77d2f38e102ac923d23
algamanser <dondokov.gunga@bwg-io.site>
2025-03-05 00:04:51 +0300

    добавил middleware next-js для определения мобилки

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const userAgent = request.headers.get('user-agent') || '';
+
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  if (isMobile) {
+    return NextResponse.redirect('https://m.godzillasoft.ru');
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/:path*',
+};
