4f7c216b1a69b36dae782ef844d564b109983886
algamanser <dondokov.gunga@bwg-io.site>
2025-04-01 00:20:27 +0300

    add yandex metrica script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import {MainPage} from "../layout/Main/Main";
 import {Metadata} from "next";
 export const metadata: Metadata = {
@@ -24,6 +25,22 @@ export const metadata: Metadata = {
 export default function Main() {
     return (
         <div>
+            <Script type="text/javascript">
+            {
+            `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(99938212, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });`
+            }
+            </Script>
             <MainPage/>
         </div>
     );
