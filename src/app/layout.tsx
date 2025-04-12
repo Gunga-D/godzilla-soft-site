bf8f6437f22b7271de35059e5106590b786d98fc
algamanser <dondokov.gunga@bwg-io.site>
2025-04-12 12:39:04 +0300

    перенес метатеги на все страницы

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,31 @@
-"use client"
 import React from 'react';
 import { Layout } from "../layout/Layout/Layout";
 import '../index.css'
 import './layoutStyled.css'
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://godzillasoft.ru'),
+    icons: {
+        icon: "/favicon.ico",
+        apple: "/favicon.ico"
+    },
+    title: 'GODZILLA SOFT — интернет-магазин видеоигр',
+    description: 'GODZILLA SOFT — интернет-магазин видеоигр и не только. Низкие цены, широкий ассортимент и моментальная доставка. Мам, купи!',
+    keywords: "купить ключи Steam, пополнить баланс Steam, цифровые товары, ключи для игр, купить игры дешево, магазин Steam ключей, пополнение Steam кошелька, цифровые подписки, игровые ключи активации, купить ключ для Steam, дешевые игры Steam, пополнить Steam мгновенно, купить подписки для игр, магазин цифровых товаров, активация ключей Steam, купить игры онлайн, пополнение Steam баланса, ключи для популярных игр, цифровые коды для игр, надежный магазин ключей",
+    openGraph: {
+        type: "website",
+        url: "/",
+        title: "GODZILLA SOFT — интернет-магазин видеоигр",
+        description: "GODZILLA SOFT — интернет-магазин видеоигр и не только. Низкие цены, широкий ассортимент и моментальная доставка. Мам, купи!",
+        siteName: "GODZILLA SOFT",
+        images: "/og_favicon_1200x675.png" ,
+    },
+    alternates: {
+        canonical: `/`,
+    },
+}
+
 interface RootLayoutProps {
     children: React.ReactNode;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,6 @@
 import Script from "next/script";
 import {MainPage} from "../layout/Main/Main";
-import {Metadata} from "next";
-export const metadata: Metadata = {
-    metadataBase: new URL('https://godzillasoft.ru'),
-    icons: {
-        icon: "/favicon.ico",
-        apple: "/favicon.ico"
-    },
-    title: 'GODZILLA SOFT — интернет-магазин видеоигр',
-    description: 'GODZILLA SOFT — интернет-магазин видеоигр и не только. Низкие цены, широкий ассортимент и моментальная доставка. Мам, купи!',
-    keywords: "купить ключи Steam, пополнить баланс Steam, цифровые товары, ключи для игр, купить игры дешево, магазин Steam ключей, пополнение Steam кошелька, цифровые подписки, игровые ключи активации, купить ключ для Steam, дешевые игры Steam, пополнить Steam мгновенно, купить подписки для игр, магазин цифровых товаров, активация ключей Steam, купить игры онлайн, пополнение Steam баланса, ключи для популярных игр, цифровые коды для игр, надежный магазин ключей",
-    openGraph: {
-        type: "website",
-        url: "/",
-        title: "GODZILLA SOFT — интернет-магазин видеоигр",
-        description: "GODZILLA SOFT — интернет-магазин видеоигр и не только. Низкие цены, широкий ассортимент и моментальная доставка. Мам, купи!",
-        siteName: "GODZILLA SOFT",
-        images: [{ url: "/og_favicon_1200x675.png" }],
-    },
-    alternates: {
-        canonical: `/`,
-    },
-}
+
 export default function Main() {
     return (
         <div>
